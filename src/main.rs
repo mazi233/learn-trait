07440b8d7323ae234025ac8158c62ff8a3009115
mazi <602281666@qq.com>
2021-11-29 16:06:01 +0800

    trait动态分发

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::SystemTime;
+
 struct Circle;
 struct Square;
 
@@ -7,24 +9,38 @@ trait Draw {
 
 impl Draw for Circle {
     fn draw(&self) {
-        println!("circle");
+        // println!("circle");
     }
 }
 
 impl Draw for Square {
     fn draw(&self) {
-        println!("square");
+        // println!("square");
     }
 }
 
-fn f(shape: &impl Draw) {
-    shape.draw();
+fn f<T: Draw>(shape1: &T, shape2: &T) {
+    shape1.draw();
+    shape2.draw();
 }
 
 fn main() {
-    let circle = Circle;
-    let square = Square;
+    // trait object的创建：指针（eg：引用，智能指针）+ dyn关键字 + 要实现的trait（一个或多个，trait之间用+连接）
+    // eg：&dyn Trait、Box<dyn Trait>。
+    // 意思就是：只要一个类型实现了指定的trait(s)，即满足该约束
 
-    f(&circle);
-    f(&square);
+    let start_time = SystemTime::now();
+
+    let box_circle: Box<dyn Draw> = Box::new(Circle);
+    let box_square: Box<dyn Draw> = Box::new(Square);
+
+    let shapes: Vec<Box<dyn Draw>> = vec![box_circle, box_square];
+
+    for _ in 0..20_000_000 as u64 {
+        for shape in shapes.iter() {
+            shape.draw();
+        }
+    }
+
+    println!("use time: {}", start_time.elapsed().unwrap().as_millis());
 }
